840b4e063f3a857f52e6ce8599c2dab874555303
N1tr0N1x <nizar.zitouni@univ-constantine2.dz>
2021-03-20 17:16:22 +0100

    Course Config with Eureka

--- src/main/java/com/eler/ms/course/CourseService/CourseServiceApplication.java
+++ src/main/java/com/eler/ms/course/CourseService/CourseServiceApplication.java
@@ -2,12 +2,22 @@ package com.eler.ms.course.CourseService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseServiceApplication.class, args);
 	}
+
+//	@RequestMapping(value = "")
+//	public String home() {
+//		return "Eureka Client application";
+//	}
 
 }
