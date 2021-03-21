1b70ecae618a78f40e4eccd14f48fbe8e73c661d
seryas17 <seryas.ratised@gmail.com>
2021-03-21 14:33:12 +0100

    centralise config - course

--- src/main/java/com/eler/ms/course/CourseService/CourseServiceApplication.java
+++ src/main/java/com/eler/ms/course/CourseService/CourseServiceApplication.java
@@ -2,13 +2,10 @@ package com.eler.ms.course.CourseService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-//@EnableEurekaClient
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
