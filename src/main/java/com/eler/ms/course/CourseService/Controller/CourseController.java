5ee5d532a0d56f7fcb42fc46e23bd9f233b3ae28
seryas17 <seryas.ratised@gmail.com>
2021-03-24 22:33:00 +0100

    fix prob

--- src/main/java/com/eler/ms/course/CourseService/Controller/CourseController.java
+++ src/main/java/com/eler/ms/course/CourseService/Controller/CourseController.java
@@ -1,26 +1,16 @@
 package com.eler.ms.course.CourseService.Controller;
 import java.util.*;
-import java.util.stream.*;
 import com.google.gson.*;
 import com.eler.ms.course.CourseService.Model.Module;
 import com.eler.ms.course.CourseService.Service.CourseService;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
-import javax.ws.rs.core.MediaType;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpMethod;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,7 +23,7 @@ public class CourseController {
     @RequestMapping(value = "/save", method = RequestMethod.POST,headers = "Accept=application/json")
     @ResponseBody
 	public String saveModule(@RequestBody Module module) {
-	Module m=course.save(module);
+	course.save(module);
 	List<Module> modules = course.listAll();
     String json = new Gson().toJson(modules);
     RestTemplate restTemplate = new RestTemplate();
