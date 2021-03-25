4bc1d179234232bc898f5192430df1d3eb64667f
ramiala <61067820+ramiala@users.noreply.github.com>
2021-03-25 22:05:19 +0100

    Update CourseControllerFiles.java

--- src/main/java/com/eler/ms/course/CourseService/Controller/CourseControllerFiles.java
+++ src/main/java/com/eler/ms/course/CourseService/Controller/CourseControllerFiles.java
@@ -24,50 +24,7 @@ public class CourseControllerFiles {
     //@RequestMapping(value = "/save", method = RequestMethod.POST,headers = "Accept=application/json")
     @PostMapping("/save")
 	public void saveFile(@RequestBody File file) {
-	    course.save(module);
+	    course.saveFile(module);
     }
 
-    @GetMapping(value = "/getModule/{id}")
-    @ResponseBody
-    public Module getModule(@PathVariable("id") int id){
-        return course.get(id);
-    }
-
-    @GetMapping(value = "/modules")
-    @ResponseBody
-    public String getAllModules() {
-        List<Module> modules = course.listAll();
-        String json = new Gson().toJson(modules);
-        return json;
-    }
-
-    @GetMapping(value = "/delete/{id}")
-    @ResponseBody
-    public void deleteModule(@PathVariable("id") int id) {
-        course.delete(id);
-    }
-
-    @GetMapping(value = "/modules/{email}/cours")
-    @ResponseBody
-    public String getModulesByEmailCours(@PathVariable("email") String email) {
-        List<Module> modules = course.getModulesByEmailCours(email);
-        String json = new Gson().toJson(modules);
-        return json;
-    }
-
-    @GetMapping(value = "/modules/{email}/td")
-    @ResponseBody
-    public String getModulesByEmailTd(@PathVariable("email") String email) {
-        List<Module> modules = course.getModulesByEmailTd(email);
-        String json = new Gson().toJson(modules);
-        return json;
-    }
-
-    @GetMapping(value = "/modules/{email}/tp")
-    @ResponseBody
-    public String getModulesByEmailTp(@PathVariable("email") String email) {
-        List<Module> modules = course.getModulesByEmailTp(email);
-        String json = new Gson().toJson(modules);
-        return json;
-    }
 }
